661e5e23f433fefa4bf8dfc215cfa2b916db1531
Natanael Fonseca <natanael.vaz@gmail.com>
2019-01-02 18:40:55 -0400

    Criando filtro responsavel por gerar o correlation id

---
+++ src/main/java/br/com/treinamento/gateway/filters/CorrelationIdPostFilter.java
@@ -0,0 +1,39 @@
+package br.com.treinamento.gateway.filters;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+
+@Component
+public class CorrelationIdPostFilter extends ZuulFilter {
+
+	private static final int FILTER_ORDER = 1;
+	private static final boolean SHOULD_FILTER = true;
+
+	@Autowired
+	FilterUtils filterUtils;
+
+	@Override
+	public String filterType() {
+		return FilterUtils.POST_FILTER_TYPE;
+	}
+
+	@Override
+	public int filterOrder() {
+		return FILTER_ORDER;
+	}
+
+	@Override
+	public boolean shouldFilter() {
+		return SHOULD_FILTER;
+	}
+
+	@Override
+	public Object run() {
+		RequestContext ctx = RequestContext.getCurrentContext();
+		ctx.getResponse().addHeader(FilterUtils.CORRELATION_ID, filterUtils.getCorrelationId());
+		return null;
+	}
+}
