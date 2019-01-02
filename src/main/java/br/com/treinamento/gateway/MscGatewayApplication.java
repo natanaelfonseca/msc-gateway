e4626389de7b9e7416ceade2bc563426475ae0e6
Natanael Fonseca <natanael.vaz@gmail.com>
2019-01-02 14:06:46 -0400

    First Commit

---
+++ src/main/java/br/com/treinamento/gateway/MscGatewayApplication.java
@@ -0,0 +1,24 @@
+package br.com.treinamento.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableZuulProxy
+public class MscGatewayApplication {
+	
+    @LoadBalanced
+    @Bean
+    public RestTemplate getRestTemplate(){
+        return new RestTemplate();
+    }
+
+	public static void main(String[] args) {
+		SpringApplication.run(MscGatewayApplication.class, args);
+	}
+
+}
